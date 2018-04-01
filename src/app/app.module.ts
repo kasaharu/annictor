68cdc8d9c70bfd55053f96809176312357914ed3
Wataru.Kasahara <kasahara1019@gmail.com>
2018-04-01 18:43:04 +0900

    [#11] AnimeService を provider に追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AnimeService } from './services/anime.service';
+
 import { AppComponent } from './app.component';
 import { AnimeListComponent } from './componets/anime-list/anime-list.component';
-
 
 @NgModule({
   declarations: [
@@ -15,7 +16,7 @@ import { AnimeListComponent } from './componets/anime-list/anime-list.component'
     BrowserModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [AnimeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
