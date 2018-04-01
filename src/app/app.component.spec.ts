80e6a5a7d2f02f99edb730d7d945b06ca3ca4bd3
Wataru.Kasahara <kasahara1019@gmail.com>
2018-04-01 16:57:35 +0900

    [#12] header 領域に title を表示

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Annictor'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Annictor');
   }));
-  it('should render title in a h1 tag', async(() => {
+  it('should render title', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Annictor!');
+    expect(compiled.querySelector('header').textContent).toContain('Annictor');
   }));
 });
