5f659a068b505a15a151ebee8229933ca438f02d
Wataru.Kasahara <kasahara1019@gmail.com>
2018-04-01 18:50:10 +0900

    [#11] AnimeListComponent を AppComponent 経由で表示

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { AnimeListComponent } from './componets/anime-list/anime-list.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        AnimeListComponent
       ],
     }).compileComponents();
   }));
