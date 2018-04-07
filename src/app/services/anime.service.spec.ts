38a853b95531f60b33330c9a715b91061d0c6691
Wataru.Kasahara <kasahara1019@gmail.com>
2018-04-07 14:39:31 +0900

    [#11] AnimeServiceTest 内で HttpClientModule を import

--- src/app/services/anime.service.spec.ts
+++ src/app/services/anime.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AnimeService } from './anime.service';
 
 describe('AnimeService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       providers: [AnimeService]
     });
   });
