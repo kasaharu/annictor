b1c8759501f7bf0ce5a69244984c36cc502ac115
Wataru.Kasahara <kasahara1019@gmail.com>
2018-04-07 20:11:52 +0900

    [#16] ANNICT_API_KEY に environment files から読み込んだ値をセット

--- src/app/services/anime.service.ts
+++ src/app/services/anime.service.ts
@@ -3,11 +3,12 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 
 import { Annict } from '../models/annict';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class AnimeService {
   ANNICT_API_PATH = 'https://api.annict.com/v1/works';
-  ANNICT_API_KEY = '';
+  ANNICT_API_KEY = environment.annictApiKey;
 
   constructor(private http: HttpClient) { }
 
