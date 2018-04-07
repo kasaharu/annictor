14618adf9631098b8ef13a15eefc5b26421177ac
Wataru.Kasahara <kasahara1019@gmail.com>
2018-04-07 11:28:18 +0900

    [#11] AnimeDetail モデルの定義

--- src/app/models/annict.ts
+++ src/app/models/annict.ts
@@ -1,2 +1,10 @@
-export interface Annict {
+export declare namespace Annict {
+  export interface AnimeDetail {
+    id: number;
+    title: string;
+    official_site_url: string;
+    title_kana: string;
+    season_name: string;
+    season_name_text: string;
+  }
 }
