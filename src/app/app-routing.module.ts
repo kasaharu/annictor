24ba0bda4dea08dc6c0b3e9a7715dd2f3abea72c
Wataru.Kasahara <kasahara1019@gmail.com>
2018-05-12 21:21:27 +0900

    [#43] ルーターの初期化

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', children: [] },
+];
 
 @NgModule({
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
