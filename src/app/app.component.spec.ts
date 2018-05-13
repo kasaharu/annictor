0deacaa87bf14e3625906a635efa00726dfbd990
Wataru.Kasahara <kasahara1019@gmail.com>
2018-05-13 19:30:15 +0900

    [#49] ルーティング用のテストモジュールを追加

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 import { MatSelectModule } from '@angular/material/select';
 import { AppComponent } from './app.component';
 import { AnimeListComponent } from './componets/anime-list/anime-list.component';
@@ -14,7 +15,7 @@ describe('AppComponent', () => {
         AnimeListComponent,
         SeasonComponent,
       ],
-      imports: [HttpClientModule, MatSelectModule],
+      imports: [HttpClientModule, MatSelectModule, RouterTestingModule],
       providers: [AnimeService],
     }).compileComponents();
   }));
