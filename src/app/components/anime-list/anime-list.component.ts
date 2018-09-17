7420bf30396908a5c8b6a77fb17680f08c68c19e
Wataru.Kasahara <kasahara1019@gmail.com>
2018-09-17 11:35:04 +0900

    [Issue#95] 指定がない場合今期のアニメ一覧を表示するように変更

--- src/app/components/anime-list/anime-list.component.ts
+++ src/app/components/anime-list/anime-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { AnimeService } from '../../services/anime.service';
 import { PeriodService } from '../../services/period.service';
@@ -14,10 +15,19 @@ export class AnimeListComponent implements OnInit {
   seasonId: string;
   animeList: Annict.AnimeDetail[];
 
-  constructor(private animeService: AnimeService, private periodService: PeriodService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private animeService: AnimeService,
+    private periodService: PeriodService,
+  ) { }
 
   ngOnInit() {
-    this.seasonId = this.periodService.getThisPeriod();
+    this.seasonId = this.route.snapshot.paramMap.get('seasonId');
+    if (!this.seasonId) {
+      this.seasonId = this.periodService.getThisPeriod();
+      this.router.navigate(['/anime-list', this.seasonId]);
+    }
     this.fetchAnimeList();
   }
 
