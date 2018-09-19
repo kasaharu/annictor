68bc072d7e7f47597dce151361009ff7f2b429f8
Wataru.Kasahara <kasahara1019@gmail.com>
2018-09-19 23:43:47 +0900

    [Issue#109] StoreModule と EffectsModule のインポート

--- src/app/root-store/root-store.module.ts
+++ src/app/root-store/root-store.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   imports: [
     CommonModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
   ],
   declarations: [],
 })
