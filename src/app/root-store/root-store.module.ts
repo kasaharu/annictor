3f286b27e607a84a184203b62276d5556b853630
Wataru.Kasahara <kasahara1019@gmail.com>
2018-09-20 22:45:42 +0900

    [Issue#109] Generate AnimeStoreModule by CLI command

--- src/app/root-store/root-store.module.ts
+++ src/app/root-store/root-store.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { AnimeStoreModule } from './anime-store/anime-store.module';
 
 @NgModule({
   imports: [
     CommonModule,
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
+    AnimeStoreModule,
   ],
   declarations: [],
 })
