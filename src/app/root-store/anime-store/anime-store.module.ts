a9eae876cb55927829b41738a349c3d4cc588be1
Wataru.Kasahara <kasahara1019@gmail.com>
2018-09-22 12:08:30 +0900

    [Issue#109] AnimeStoreModule に StoreModule をインポート

--- src/app/root-store/anime-store/anime-store.module.ts
+++ src/app/root-store/anime-store/anime-store.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+
+import { animeReducer } from './reducers';
 
 @NgModule({
   imports: [
     CommonModule,
+    StoreModule.forFeature('animeList', animeReducer),
   ],
   declarations: [],
 })
