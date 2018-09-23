c07f6be3a75db526c2162b0b5f8e17c36e927e80
Wataru.Kasahara <kasahara1019@gmail.com>
2018-09-23 14:44:23 +0900

    [Issue#109] AnimeStoreEffects の実装

---
+++ src/app/root-store/anime-store/effects.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Action, Store, select } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+import { mergeMap, map, tap, withLatestFrom } from 'rxjs/operators';
+
+import * as AnimeStoreActions from './actions';
+import { selectAnimeListState } from './selectors';
+import { State } from './state';
+
+import { AnimeService } from '../../services/anime.service';
+
+@Injectable()
+export class AnimeStoreEffects {
+  constructor(
+    private actions$: Actions,
+    private store: Store<State>,
+    private animeService: AnimeService,
+  ) {}
+
+  @Effect()
+  fetch$: Observable<Action> = this.actions$.pipe(
+    ofType(AnimeStoreActions.ActionTypes.FETCH_REQUEST),
+    mergeMap(_ =>
+      this.animeService.fetchAnimeList('2019-winter').pipe(
+        map(result => new AnimeStoreActions.FetchSuccessAction(result)),
+      ),
+    ),
+  );
+}
