4ade1971eacbbd223f4546b832d3d33f881afa6d
Wataru.Kasahara <kasahara1019@gmail.com>
2018-09-23 13:46:10 +0900

    [Issue#109] Actions のインポート名をリネーム

--- src/app/root-store/anime-store/index.ts
+++ src/app/root-store/anime-store/index.ts
@@ -1,6 +1,6 @@
-import * as AnimeStoreAction from './actions';
+import * as AnimeStoreActions from './actions';
 import * as AnimeStoreSelectors from './selectors';
 import * as AnimeStoreState from './state';
 
 export { AnimeStoreModule } from './anime-store.module';
-export { AnimeStoreAction, AnimeStoreSelectors, AnimeStoreState };
+export { AnimeStoreActions, AnimeStoreSelectors, AnimeStoreState };
