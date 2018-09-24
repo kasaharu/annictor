4b3792ef67316770f13a3af9c060f974e2170d82
Wataru.Kasahara <kasahara1019@gmail.com>
2018-09-24 17:20:56 +0900

    [Issue#109] Fix spec: 必要なサービスを登録

--- src/app/containers/works/works.component.spec.ts
+++ src/app/containers/works/works.component.spec.ts
@@ -1,20 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { WorksComponent } from './works.component';
 
+import { ActivatedRouteStub } from '../../testing/activated-route-stub';
 import { RootStoreModule } from '../../root-store';
 import { AnimeService } from '../../services/anime.service';
 
 describe('WorksComponent', () => {
   let component: WorksComponent;
   let fixture: ComponentFixture<WorksComponent>;
+  const activatedRoute = new ActivatedRouteStub({ seasonId: '' });
 
   beforeEach(async(() => {
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
     TestBed.configureTestingModule({
       declarations: [ WorksComponent ],
       imports: [HttpClientModule, RootStoreModule],
-      providers: [AnimeService],
+      providers: [
+        AnimeService,
+        { provide: Router, useValue: routerSpy },
+        { provide: ActivatedRoute, useValue: activatedRoute },
+      ],
     })
     .compileComponents();
   }));
