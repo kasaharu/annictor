ae57f6e8183aa733622042b37e646bc640e27866
Wataru.Kasahara <kasahara1019@gmail.com>
2018-09-28 22:06:14 +0900

    [Issue#109] Season の ActionCreator を実装

--- src/app/root-store/season-store/actions.ts
+++ src/app/root-store/season-store/actions.ts
@@ -1,4 +1,19 @@
+import { Action } from '@ngrx/store';
+import { Season } from '../../core/models';
+
 export enum ActionTypes {
   FETCH_REQUEST = '[Season] Fetch Request',
   FETCH_SUCCESS = '[Season] Fetch Success',
 }
+
+export class FetchRequstAction implements Action {
+  readonly type = ActionTypes.FETCH_REQUEST;
+  constructor(public payload: string) {}
+}
+
+export class FetchSuccessAction implements Action {
+  readonly type = ActionTypes.FETCH_SUCCESS;
+  constructor(public payload: Season) {}
+}
+
+export type ActionsUnion = FetchRequstAction | FetchSuccessAction;
