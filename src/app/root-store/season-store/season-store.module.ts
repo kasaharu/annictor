09caba5a34fc74ae2448b011231140e01e4ca25b
Wataru.Kasahara <kasahara1019@gmail.com>
2018-09-30 15:18:37 +0900

    [Issue#109] SeasonStoreModule に StoreModule をインポート

--- src/app/root-store/season-store/season-store.module.ts
+++ src/app/root-store/season-store/season-store.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+
+import { seasonReducer } from './reducers';
 
 @NgModule({
   imports: [
     CommonModule,
+    StoreModule.forFeature('season', seasonReducer),
   ],
   declarations: [],
 })
