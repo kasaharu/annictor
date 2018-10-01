a16f7a0b9179ce3f4e49c8e1f0edc15fa666d086
Wataru.Kasahara <kasahara1019@gmail.com>
2018-10-01 21:12:16 +0900

    [Issue#127] AnimeService#fetchAnimeList() のテスト追加

--- src/app/services/anime.service.spec.ts
+++ src/app/services/anime.service.spec.ts
@@ -1,17 +1,31 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { AnimeService } from './anime.service';
 
 describe('AnimeService', () => {
+  let httpTestingController: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [AnimeService],
     });
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   it('should be created', inject([AnimeService], (service: AnimeService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('call fetchAnimeList() method', inject([AnimeService], (service: AnimeService) => {
+    const filterSeason = '2018-autumn';
+    const targetUrl =
+      `${service.ANNICT_API_PATH}?access_token=${service.ANNICT_API_KEY}&filter_season=${filterSeason}&sort_watchers_count=desc`;
+
+    service.fetchAnimeList(filterSeason).subscribe();
+    const req = httpTestingController.expectOne(targetUrl);
+    expect(req.request.method).toEqual('GET');
+    httpTestingController.verify();
   }));
 });
