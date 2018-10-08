5d182b73dda03ffbad0693c843a0b153e6c252af
Wataru.Kasahara <kasahara1019@gmail.com>
2018-10-08 09:14:33 +0900

    [Issue#139] WorksComponent#fetchAnimeList() のテストを実装

--- src/app/containers/works/works.component.spec.ts
+++ src/app/containers/works/works.component.spec.ts
@@ -1,17 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { Store } from '@ngrx/store';
 
 import { WorksComponent } from './works.component';
 
 import { ActivatedRouteStub } from '../../testing/activated-route-stub';
-import { RootStoreModule } from '../../root-store';
+import { RootStoreModule, RootStoreState, AnimeStoreActions } from '../../root-store';
 import { AnimeListComponent } from '../../components/anime-list/anime-list.component';
 import { AnimeService } from '../../services/anime.service';
 
 describe('WorksComponent', () => {
   let component: WorksComponent;
   let fixture: ComponentFixture<WorksComponent>;
+  let store$: Store<RootStoreState.State>;
   const activatedRoute = new ActivatedRouteStub({ seasonId: '' });
 
   beforeEach(async(() => {
@@ -26,6 +28,8 @@ describe('WorksComponent', () => {
       ],
     })
     .compileComponents();
+    store$ = TestBed.get(Store);
+    spyOn(store$, 'dispatch').and.callThrough();
   }));
 
   beforeEach(() => {
@@ -41,5 +45,13 @@ describe('WorksComponent', () => {
   it('URL path に seasonId を持っている場合', () => {
     activatedRoute.setParamMap({ seasonId: '2020-spring' });
     expect(component).toBeTruthy();
+  });
+
+  it('call fetchAnimeList() method', () => {
+    const seasonId = '2020-spring';
+    const action = new AnimeStoreActions.FetchRequstAction(seasonId)
+    component.fetchAnimeList(seasonId);
+
+    expect(store$.dispatch).toHaveBeenCalledWith(action);
   });
 });
