3b6513b44564a7860c16afa50b864be249971130
Wataru.Kasahara <kasahara1019@gmail.com>
2018-10-21 17:29:00 +0900

    [Issue#178] Service Worker の機能を OFF にする

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServiceWorkerModule } from '@angular/service-worker';
+// import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 
-import { environment } from '../environments/environment';
+// import { environment } from '../environments/environment';
 
 import { AnimeService } from './services/anime.service';
 import { PeriodService } from './services/period.service';
@@ -32,7 +32,8 @@ import { WorksComponent } from './containers/works/works.component';
     HttpClientModule,
     MatButtonModule,
     MatSelectModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    // TODO: Service Worker の設定を一度見直すため機能を OFF にする
+    // ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     AppRoutingModule,
     RootStoreModule,
   ],
