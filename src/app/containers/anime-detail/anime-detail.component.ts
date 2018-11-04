b440f78e2870206eeb31141abe14bf9f298ff051
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-04 13:23:47 +0900

    [Issue#201] Generate AnimeDetailComponent by CLI command

---
+++ src/app/containers/anime-detail/anime-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anime-detail',
+  templateUrl: './anime-detail.component.html',
+  styleUrls: ['./anime-detail.component.scss']
+})
+export class AnimeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
