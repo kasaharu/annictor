f4aad58f4a715720f7273d7289b98f921f6f8ff7
Wataru.Kasahara <kasahara1019@gmail.com>
2018-11-04 13:30:31 +0900

    [Issue#201] AnimeDetailComponent へのルーティングを設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AnimeDetailComponent } from './containers/anime-detail/anime-detail.component';
 import { WorksComponent } from './containers/works/works.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/works', pathMatch: 'full' },
   { path: 'works', component: WorksComponent },
   { path: 'works/:seasonId', component: WorksComponent },
+  { path: 'anime-detail/:workId', component: AnimeDetailComponent },
 ];
 
 @NgModule({
